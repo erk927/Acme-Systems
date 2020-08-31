52702b046176eeb38a23c8e5435e30c26a81da9d
erk927 <70233197+erk927@users.noreply.github.com>
2020-08-30 20:45:01 -0400

    Testing Intelli-J git func

--- Acme Systems/src/ver1/HelloDolly.java
+++ Acme Systems/src/ver1/HelloDolly.java
@@ -6,6 +6,7 @@ public class HelloDolly {
 		// TODO Auto-generated method stub
 		System.out.println("Helloooooo Dolly");
 		System.out.println("Hello Dolly from Eclipse Again");
+		System.out.println("Hello From intelli-J");
 	}
 
 }
