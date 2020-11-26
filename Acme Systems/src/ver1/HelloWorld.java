2d0a7c8a95b21974fe061b4efbb471f99c357c99
erk927 <densonerskine@gmail.com>
2020-11-26 16:30:50 -0500

    resolved conflict

--- Acme Systems/src/ver1/HelloWorld.java
+++ Acme Systems/src/ver1/HelloWorld.java
@@ -4,11 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< Upstream, based on origin/master
-		System.out.println("Hello from Eclipse // Edited master in eclipse. Going to rebase this onto patch-3 and see what happens");
-=======
-		System.out.println("Hello from Eclipse // Edited patch-3 in eclipse. Going to edit master in eclipse then rebase master onto this patch-2");
->>>>>>> db6e027 edited helloWorld again
+		System.out.println("Hello from Eclipse // Resolved conflict in eclipse with rebase");
 		System.out.println("Hello from GitHub");
 	}
 
