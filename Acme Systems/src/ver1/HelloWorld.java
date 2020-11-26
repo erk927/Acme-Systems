7caf825a2e41d775c33161ebb99eee042ba538d0
erk927 <70233197+erk927@users.noreply.github.com>
2020-11-26 16:07:00 -0500

    Update HelloWorld.java

--- Acme Systems/src/ver1/HelloWorld.java
+++ Acme Systems/src/ver1/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello from Eclipse");
-		System.out.println("Hello from GitHub // Resolve conflict: This was supposed to allow me to resolve the issue in eclipse");
+		System.out.println("Hello from GitHub // This should cause a collision");
 	}
 
 	public static void heyNow() {
