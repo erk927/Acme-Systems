1f1b00732fa50799057b0c215ce5f9460176160b
erk927 <70233197+erk927@users.noreply.github.com>
2020-11-26 16:35:53 -0500

    Update MyaAme.java

--- Acme Systems/src/ver1/MyaAme.java
+++ Acme Systems/src/ver1/MyaAme.java
@@ -4,7 +4,7 @@ public class MyaAme {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("My name is Earl");
+		System.out.println("My name is Earl // just going to leave this here for you");
 	}
 
 }
