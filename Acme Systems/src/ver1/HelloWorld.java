7b04e3e6836de6e65717ed669ad21bf20b58e173
erk927 <densonerskine@gmail.com>
2020-11-26 16:38:19 -0500

    Edit three different files in the same package. should cause conflict

--- Acme Systems/src/ver1/HelloWorld.java
+++ Acme Systems/src/ver1/HelloWorld.java
@@ -4,12 +4,12 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello from Eclipse // Edited this line in github");
-		System.out.println("Hello from GitHub // edited this line in github as weel");
+		System.out.println("Hello from Eclipse // Resolved conflict with rebase");
+		System.out.println("Hello from GitHub // Resolved conflict with rebase");
 	}
 
 	public static void heyNow() {
-		String Msg = "Hey Now! // This code isn't being used, but I 'm going to edit it anyway";
+		String Msg = "Hey Now! // Resolved conflict with rebase";
 		System.out.println(Msg);
 	}
 
