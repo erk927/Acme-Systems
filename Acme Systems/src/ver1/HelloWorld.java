85fa80fa6863524592276744207ba38dfd646133
erk927 <densonerskine@gmail.com>
2020-11-26 15:54:08 -0500

    edited HelloWorld

--- Acme Systems/src/ver1/HelloWorld.java
+++ Acme Systems/src/ver1/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello from Eclipse");
-		System.out.println("Hello from GitHub");
+		System.out.println("Hello from GitHub // Edited this line on 11/26 from eclipse after editing it in github. This should cause a colision");
 	}
 
 	public static void heyNow() {
