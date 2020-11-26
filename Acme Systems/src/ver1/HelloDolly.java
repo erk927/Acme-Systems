804c90a0e21d3d0218ee534bbc1e7e3c68ac51dd
erk927 <70233197+erk927@users.noreply.github.com>
2020-11-26 16:35:30 -0500

    Update HelloDolly.java

--- Acme Systems/src/ver1/HelloDolly.java
+++ Acme Systems/src/ver1/HelloDolly.java
@@ -5,8 +5,8 @@ public class HelloDolly {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Helloooooo Dolly");
-		System.out.println("Going to see what happerns here");
-		System.out.println("Hello From intelli-J");
+		System.out.println("Going to see what happerns here // Edited this in github along with some lines in helloworld");
+		System.out.println("Hello From intelli-J // I never maed it to intelli-j, but I did edit this line in github though");
 	}
 
 }
