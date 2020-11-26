0f0896ea83c78d049efcf65744219eeea8723315
erk927 <densonerskine@gmail.com>
2020-11-26 15:48:19 -0500

    changed hellodolly

--- Acme Systems/src/ver1/HelloDolly.java
+++ Acme Systems/src/ver1/HelloDolly.java
@@ -5,7 +5,7 @@ public class HelloDolly {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Helloooooo Dolly");
-		System.out.println("Hello Dolly from Eclipse Again");
+		System.out.println("Going to see what happerns here");
 		System.out.println("Hello From intelli-J");
 	}
 
