5d58a2b7fd94918582466732788b5cb4583af0dc
erk927 <70233197+erk927@users.noreply.github.com>
2020-12-06 14:57:13 -0500

    Update HelloDolly.java

--- Acme Systems/src/ver1/HelloDolly.java
+++ Acme Systems/src/ver1/HelloDolly.java
@@ -4,9 +4,9 @@ public class HelloDolly {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Helloooooo Dolly");
+		System.out.println("Helloooooo Dolly // Lets see what shows up on which side");
 		System.out.println("Going to see what happerns here // Resolved conflict with rebase");
-		System.out.println("Hello From intelli-J // Resolved conflict with rebase");
+		System.out.println("Hello From intelli-J // This should cause a conflict");
 	}
 
 }
