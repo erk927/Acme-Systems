a1ba097125e828069255e1293b2601b3594125c0
erk927 <70233197+erk927@users.noreply.github.com>
2020-12-06 14:58:09 -0500

    Update HelloWorld.java

--- Acme Systems/src/ver1/HelloWorld.java
+++ Acme Systems/src/ver1/HelloWorld.java
@@ -4,8 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello from Eclipse // Resolved conflict with rebase");
-		System.out.println("Hello from GitHub // Resolved conflict with rebase");
+		System.out.println("Hello from Eclipse // Conflict from master");
+		System.out.println("Hello from GitHub // Ryabba dabba doo");
 	}
 
 	public static void heyNow() {
